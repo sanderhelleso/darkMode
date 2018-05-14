ff9c6bc21edee35c3d4782792dcec04804ae4295
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-14 06:21:02 +0200

    update

--- darkmode.js
+++ darkmode.js
@@ -7,15 +7,19 @@
 
 ******************************************/
 
+// Globals
+var body;
 var textColor;
 var backgroundColor:
+
+// start the application
 window.onload = start;
 function start() {
 
-    // initialize darkmode trigger, eplace trigger with your own trigger element
+    // initialize darkmode trigger, replace trigger with your own trigger element
     document.getElementById("trigger").addEventListener("click", darkMode);
 
-    // to change colors, replace the following color codes with your own
+    // to change colors, replace the following colorcodes with your own
     textColor = "#9e9e9e";
     backgroundColor = "#212121";
 }
@@ -26,29 +30,37 @@ function darkMode() {
     this.addEventListener("click", clearDarkMode);
 
     // set body as dark
-    const body = document.getElementById("body");
+    body = document.getElementById("body");
     body.style.backgroundColor = backgroundColor;
 
     // get all elements on the page
     document.querySelectorAll("*").forEach(function(node) {
 
         // text elements
-        let text = node.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, span");
+        let text = node.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, li, span");
         text.forEach(function(ele) {
             ele.style.color = textColor;
-            darkEles.push(ele);
+            darkElements.push(ele);
+        });
+
+        // buttons
+        let buttons = node.querySelectorAll("button");
+        buttons.forEach(function(ele) {
+            ele.style.backgroundColor = backgroundColor;
+            ele.style.color = textColor;
+            darkElements.push(ele);
         });
 
         // containers
-        let containers = node.querySelectorAll("header, section, main, footer, div, nav, a, button");
+        let containers = node.querySelectorAll("header, section, main, footer, div, nav");
         containers.forEach(function(ele) {
             ele.style.backgroundColor = backgroundColor;
-            darkEles.push(ele);
+            darkElements.push(ele);
         });
     });
 }
 
-var darkEles = [];
+var darkElements = [];
 function clearDarkMode() {
 
     // init darkMode
@@ -62,9 +74,8 @@ function clearDarkMode() {
     });
 
     // revert body
-    const body = document.getElementById("body");
     body.style.removeProperty("background-color");
 
     // reset array containing changed elements
-    darkEles = [];
+    darkElements = [];
 }
